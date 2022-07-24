541abba8dfacd9e10b39d3146b4c4abc9f483ac5
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-07-24 15:24:37 -0300

    Created component Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -9,10 +10,7 @@ export function Sidebar() {
         src='https://images.unsplash.com/photo-1537884944318-390069bb8665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src='https://avatars.githubusercontent.com/u/33641830?v=4'
-        />
+        <Avatar src='https://github.com/LMThomaz-Study.png' />
         <strong>Leonardo Thomaz</strong>
         <span>Web Developer</span>
       </div>
