223dea99d971d9e220d0b9b7e3c01828b3f0cfc8
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-07-24 19:59:06 -0300

    pass props to component Post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,57 @@ import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/LMThomaz.png',
+      name: 'Leonardo Thomaz',
+      role: 'Web Developer',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: '👉 jane.design/doctorcare',
+      },
+    ],
+    publishedAt: new Date('2022-07-10 10:10:10'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://github.com/LMThomaz-Study.png',
+      name: 'Leonardo Study',
+      role: 'Web Developer',
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋',
+      },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      {
+        type: 'link',
+        content: '👉 jane.design/doctorcare',
+      },
+    ],
+    publishedAt: new Date('2022-07-09 10:10:10'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -11,8 +62,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,15 +2,15 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://github.com/LMThomaz.png' />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Leonardo Thomaz</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
         <time title='24 de Julho às 11:30h' dateTime='2022-07-24 11:30:00'>
@@ -45,8 +45,6 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
         <Comment />
       </div>
     </article>
