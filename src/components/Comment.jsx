fc8e342a7b069d775677e5abf32c2d5793f0974e
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-07-28 20:42:43 -0300

    relized likes in comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src='https://github.com/LMThomaz.png' />
@@ -17,18 +26,16 @@ export function Comment({ content, onDeleteComment }) {
                 Cerca de 1h atrás
               </time>
             </div>
-            <button
-              onClick={() => onDeleteComment(content)}
-              title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
